658e889d68b4ed705af4d6b07f9fd0f0fc12f28d
Ricky Arora <rtyer3@gmail.com>
2019-09-18 16:35:54 -0400

    working on the Ghost game

--- W1D1/Ghost/game.rb
+++ W1D1/Ghost/game.rb
@@ -3,9 +3,10 @@ class Game
         @fragment = ""
     end
 
-    def is_valid?(character)
+    def valid_play?(character)
         if character.count() == 1 && character.count("a-zA-Z") == 1
-        end
-        return "invalid play"
+            fragment = fragment + character 
+        else 
+            p "invalid play, try again"
     end
 end
