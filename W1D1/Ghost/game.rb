f646ddafb2123d70fe796fdc81c8769f352aab51
Ricky Arora <rtyer3@gmail.com>
2019-09-25 09:58:44 -0400

    more work on Ghost

--- W1D1/Ghost/game.rb
+++ W1D1/Ghost/game.rb
@@ -1,12 +1,13 @@
 require 'byebug'
 require 'set'
 class Game
-    attr_accessor :fragment, :players, :dictionary
-    def initialize()
+    attr_accessor :fragment, :players, :dictionary, :current_player
+    def initialize(list_of_players)
         words = File.readlines("dictionary.txt", chomp: true)
         @fragment = ""
-        @players = ""
+        @players = list_of_players
         @dictionary = words.to_set
+        @current_player = players[0]
     end
 
     def valid_play?(character)
@@ -23,22 +24,31 @@ class Game
         end
     end
 
+    def play_round 
+        # implement methods below and call them here
+    end
+
     def current_player()
+        @current_player
     end
 
     def previous_player()
+        @players[(@players.index(@current_player))-1]
     end
 
     def nextplayer!()
+        @players = @players[(@players.index(@current_player))+1]
     end
 
     def take_turn(player)
+        @current_player.guess
     end
 
     def play_around
+        # ????
     end
 
 end
 
 new_game = Game.new
-new_game.valid_play?("l")
+p new_game.valid_play?("l")
--- W1D1/Ghost/player.rb
+++ W1D1/Ghost/player.rb
@@ -1,5 +1,18 @@
 class Player
+    attr_accessor :name, :guess
+
     def initialize(name)
         @name = name
     end
+
+    def guess
+        p "Make a guess below, it must be one character"
+        guess = gets
+        guess = guess.chomp
+        alert_invalid_guess(guess)
+    end
+
+    def alert_invalid_guess
+    end
+
 end
