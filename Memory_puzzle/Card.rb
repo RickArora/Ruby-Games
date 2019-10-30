dbfb66e40dd6c43c8bfa7fd36dcd8313c77afc2c
Ricky Arora <rtyer3@gmail.com>
2019-10-30 13:54:19 -0400

    updated blueprint for the Card class

---
+++ Memory_puzzle/Card.rb
@@ -0,0 +1,21 @@
+class Card
+
+    attr_accessor :face_value, :face_up
+
+        def initialize
+            @face_value = "temp_value"
+            @face_up_or_down = false
+        end
+
+        def hide
+        end
+
+        def reveal
+        end
+
+        def to_s 
+        end
+
+        def ==
+        end
+end
