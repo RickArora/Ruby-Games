efd69f410d86be3e40ffa32d7b73d6d8defc9378
Ricky Arora <rtyer3@gmail.com>
2019-10-30 16:14:37 -0400

    further in the blueprint

--- Memory_puzzle/Board.rb
+++ Memory_puzzle/Board.rb
@@ -1,10 +1,14 @@
 class Board
 
   def initialize(array_of_cards)
-    
+    render
   end
 
   def render
+    p "0 1 2 3"
+    p "0\n1\n2\n3\n4"
+    #while array_of_cards != 0
+    #end
   end
 
   def won?
