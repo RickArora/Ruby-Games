f7e068c2cbbd43be054ad73d1c977f662db66506
Ricky Arora <rtyer3@gmail.com>
2019-10-31 12:06:28 -0400

    working on Board.rb

--- Memory_puzzle/Board.rb
+++ Memory_puzzle/Board.rb
@@ -6,7 +6,7 @@ class Board
 
   def render
     p "0 1 2 3"
-    p "0\n1\n2\n3\n4"
+    p "0\n 1\n 2\n 3\n 4"
     #while array_of_cards != 0
     #end
   end
@@ -18,3 +18,5 @@ class Board
   end
 
 end
+
+Board = Board.new([1,1,2,2,3,3,4,4,5,5,6,6,7,7,8,8,9,9,10,10,11,11,12,12])
